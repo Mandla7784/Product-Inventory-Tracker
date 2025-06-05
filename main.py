e404d48e56fae4b4bc3f2fd1ac0096e416b739dc
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-06-05 09:09:03 +0200

    Added Product clsss to create new product

---
+++ main.py
@@ -0,0 +1,11 @@
+# TODO  create a simple Python program that manages a small product inventory system
+
+
+def main()-> None:
+    pass
+
+
+
+
+if __name__=="__main__":
+    main()
---
+++ product.py
@@ -0,0 +1,17 @@
+#class calleed tracker  consist and a class called product
+
+
+class Product:
+    def __init__(self , name , category , stock ):
+        self.name = name
+        self.category  = category
+        self.stock = stock
+        
+
+
+
+
+
+
+
+
