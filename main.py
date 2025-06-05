2238b5cc10a572786b3ae81de0e15754eef9d8ae
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-06-05 09:24:22 +0200

    Added add product methof

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 # TODO  create a simple Python program that manages a small product inventory system
 from product import Product
 
+stock = [] # stock to keep track of available items
+def add_new_product(name , cater , price , quant):
+    new_product = Product(name , price , cater , quant )
+    
+    if new_product.name in stock and new_product.stock == quant:
+      choice =   print(f"{new_product.name} already exist do you want to add more ? (Y/N)")
+    
+    
+    
+    
 
 
 def main()-> None:
-    stock = [] # stock to keep track of available items
+ 
     product_name = input("Enter prduct Name")
     category = input("Category : ")
-    price =  
+    price =  float(input("Price: "))
+    quantity  = int(input("Enter quantity"))
+
 
 
 
