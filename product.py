ca12c69b4b7fa04d7ec7b3cf23cd56253b024d75
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-06-05 09:14:22 +0200

    added annotation to representation method

--- product.py
+++ product.py
@@ -1,15 +1,16 @@
 #class calleed tracker  consist and a class called product
-
-
 class Product:
     def __init__(self , name:str , category:str , stock:int ) -> None:
         self.name = name
         self.category  = category
         self.stock = stock
         
-        
+    @classmethod   
     def __repr__(self):
-        return f"Product: Name->  {self.name} {self.category} {self.stock} "
+        return f"Product: Name->  {self.name} Category -> {self.category} stock -> {self.stock} "
+    
+
+        
         
 
 
