53e06e890e8a1c7b5a7ed4a3e6eca8e70bbc25f4
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-06-06 10:13:07 +0200

    Added a set up method

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,8 @@
-from main import add_new_product , available_stock ,search_by_name ,update_stock 
 
 import unittest
+
+from main import add_new_product , available_stock ,search_by_name ,update_stock 
+from product import Product
 
 
 class TestMain(unittest.TestCase):
@@ -8,10 +10,19 @@ class TestMain(unittest.TestCase):
     def __init__(self, methodName = "runTest"):
         super().__init__(methodName)
         
+    def setUp(self):
+        available_stock.clear()
+        new_item  = Product()
+        
+            
+        
     def test_add_new_product(self):
+        
+
         pass
     
     
     
-
+if __name__=="__main__":
+    unittest.main()
         
