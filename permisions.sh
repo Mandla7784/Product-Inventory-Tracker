93c9b53a78599e169a34f5f09787b089b4ca4454
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-09-24 08:25:05 +0200

    added checking for operating system whe  building the image

--- permisions.sh
+++ permisions.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
 echo "Fixing Docker permissions..."
+echo "Checking operating system..."
 
-if groups $USER | grep -q '\bdocker\b'; then
-    echo "User is already in the docker group. Continuing..."
-else
-    sudo usermod -aG docker $USER
-    echo "You have been added to the docker group. Please log out and back in before continuing."
-    exit 1
-fi
+OS="$(uname -s)"
+
+if [[ "$OS" == "Linux"]]; then 
+    echo "Linux detected. Fixing Docker permissions..."
 
 
-# restarting 
-sudo systemctl restart docker
+    if groups $USER | grep -q '\bdocker\b'; then
+        echo "User is already in the docker group. Continuing..."
+    else
+        sudo usermod -aG docker $USER
+        echo "You have been added to the docker group. Please log out and back in before continuing."
+        exit 1
+    fi
 
 
-# Check and load veth kernel module
-lsmod | grep veth || sudo modprobe veth
-
-# Check Docker status
-sudo systemctl status docker
-
-echo "If you still see errors, consider reinstalling Docker or rebooting your system."
+    # restarting 
+    sudo systemctl restart docker
 
 
-# Build and run
-echo "buidling....."
+    # Check and load veth kernel module
+    lsmod | grep veth || sudo modprobe veth
 
-docker build -t product-inventory .
-docker run -it product-inventory
+    # Check Docker status
+    sudo systemctl status docker
+
+    echo "If you still see errors, consider reinstalling Docker or rebooting your system."
+
+
+    # Build and run
+    echo "buidling....."
+
+    docker build -t product-inventory .
+    docker run -it product-inventory
